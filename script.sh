87fea65c44d6baa1290ac39d81b7359f71dcb298
Audrey Hansrisuk <ahansris@crcfe01.crc.nd.edu>
2023-10-13 13:55:53 -0400

    trying to copy script.sh from test2 to master

--- script.sh
+++ script.sh
@@ -18,12 +18,12 @@ mkdir workingfiles
 cd workingfiles
 
 ## 4) aligning the proteome files to the reference sequences using muscle
-/afs/crc.nd.edu/user/a/ahansris/Private/tools/muscle -align $1/refseq/mcrAref.fasta -output mcrA_aligned.fasta
-/afs/crc.nd.edu/user/a/ahansris/Private/tools/muscle -align $1/refseq/hsp70ref.fasta -output hsp70_aligned.fasta
+../../../tools/muscle -align $1/refseq/mcrAref.fasta -output mcrA_aligned.fasta
+../../../tools/muscle -align $1/refseq/hsp70ref.fasta -output hsp70_aligned.fasta
 
 ## 5) building an HMM profile via hmmbuild for the two genes
-/afs/crc.nd.edu/user/a/ahansris/Private/Biocomputing/tools/hmmbuild mcrA.hmm mcrA_aligned.fasta
-/afs/crc.nd.edu/user/a/ahansris/Private/Biocomputing/tools/hmmbuild hsp70.hmm hsp70_aligned.fasta
+../../tools/hmmbuild mcrA.hmm mcrA_aligned.fasta
+../../tools/hmmbuild hsp70.hmm hsp70_aligned.fasta
 
 ## 6) looping through each proteome file with hmmsearch to identify number of genes, and nesting in its respective directory
 mkdir mcrAsearches
@@ -31,31 +31,31 @@ cd mcrAsearches
 for file in $1/proteomes/*.fasta
 do
 	numb=$(echo $file | grep -Eo '[0-9]{2}')
-	/afs/crc.nd.edu/user/a/ahansris/Private/Biocomputing/tools/hmmsearch --tblout "proteome"$numb"mcrA.txt" $1/workingfiles/mcrA.hmm $file
+	../../tools/hmmsearch --tblout "proteome"$numb"mcrA.txt" mcrA.hmm $file
 done
 
-cd $1/workingfiles
+cd $1
 mkdir hsp70searches
 cd hsp70searches
-for file in $1/proteomes/*.fasta;
+for file in $1../proteomes/*.fasta;
 do
 	numb=$(echo $file | grep -Eo '[0-9]{2}')
-	/afs/crc.nd.edu/user/a/ahansris/Private/Biocomputing/tools/hmmsearch --tblout "proteome"$numb"hsp70.txt" $1/workingfiles/hsp70.hmm $file
+	../../tools/hmmsearch --tblout "proteome"$numb"hsp70.txt" hsp70.hmm $file
 done
 
-cd $1/workingfiles
+cd $1
 
 ## 7) creating a summary file in the order: proteome # | mcrA gene counts | hsp70 gene counts
 for file in $1/proteomes/*.fasta;
 do
 	numb=$(echo $file | grep -Eo '[0-9]{2}')
-	mcrAcounts=$(grep -vc "#" "$1/workingfiles/mcrAsearches/proteome"$numb"mcrA.txt")
-	hsp70counts=$(grep -vc "#" "$1/workingfiles/hsp70searches/proteome"$numb"hsp70.txt")
-	echo "proteome_"$numb","$mcrAcounts","$hsp70counts >> summary.csv
+	mcrAcounts=$(grep -vc "#" "$1/mcrAsearches/proteome"$numb"mcrA.txt")
+	hsp70counts=$(grep -vc "#" "$1/hsp70searches/proteome"$numb"hsp70.txt")
+	echo "proteome"$numb $mcrAcounts $hsp70counts >> summary.csv
 done
 
 ## 8) creating a file of potential methanogens
-cat summary.csv | grep -wv "0" | sort -t, -k3,3nr -k1,1r >> methanogens.csv
+cat summary.csv | grep -wv "0" | sort -t, -k3,3nr >> methanogens.csv
 
 ## 9) moving the summary.txt and methanogens.txt files
 mv summary.csv methanogens.csv $1
