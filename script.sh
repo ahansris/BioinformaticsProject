28563e8ca81e96b0cf3f506c9be3531019488684
Audrey Hansrisuk <ahansris@crcfe01.crc.nd.edu>
2023-10-13 11:13:56 -0400

    changed the summary and methanogen files to csv

--- script.sh
+++ script.sh
@@ -43,7 +43,7 @@ do
 	../../tools/hmmsearch --tblout "proteome"$numb"hsp70.txt" hsp70.hmm $file
 done
 
-cd ..
+cd $1
 
 ## 7) creating a summary file in the order: proteome # | mcrA gene counts | hsp70 gene counts
 for file in $1/proteomes/*.fasta;
@@ -51,11 +51,11 @@ do
 	numb=$(echo $file | grep -Eo '[0-9]{2}')
 	mcrAcounts=$(grep -vc "#" "./mcrAsearches/proteome"$numb"mcrA.txt")
 	hsp70counts=$(grep -vc "#" "./hsp70searches/proteome"$numb"hsp70.txt")
-	echo "proteome"$numb $mcrAcounts $hsp70counts >> summary.txt
+	echo "proteome"$numb $mcrAcounts $hsp70counts >> summary.csv
 done
 
 ## 8) creating a file of potential methanogens
-cat summary.txt | grep -wv "0" | sort -t' ' -k3,3nr -k1,1 >> methanogens.txt
+cat summary.csv | grep -wv "0" | sort -t, -k3,3nr -k1,1 >> methanogens.csv
 
 ## 9) moving the summary.txt and methanogens.txt files
-mv summary.txt methanogens.txt $1
+mv summary.csv methanogens.csv $1
